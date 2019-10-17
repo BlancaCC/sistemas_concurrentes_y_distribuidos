0a8566c4f8b794c024088f7b747a9a85432ca8b0
Blanca <blancacano@correo.ugr.es>
2019-10-17 16:24:25 +0200

    examen prácticas: estancos 2

--- semaforos/estancos2.cpp
+++ semaforos/estancos2.cpp
@@ -1,7 +1,8 @@
 /**
    @autor Blanca Cano  Camarero 
+
+@brief a partir del seminario de la práctica de los estancos añadir una suministradora que coloque en un palé los ingredientes para una vez de un estanquero
    
-   @brief: un proveedor suministra a varios estanqueros que comparten un mostrador del que consumen varios fumadores 
  */
 
 #include <iostream>
@@ -30,49 +31,76 @@ template< int min, int max > int aleatorio()
 }
 
 
-const int numEstanqueros= 2, numFumadores=4; //cada fumador consume un tipo concreto de producto, un estanquero puede vender cualquiera
+const int numEstanqueros= 1, numFumadores=3; //cada fumador consume un tipo concreto de producto, un estanquero puede vender cualquiera
 
 mutex mtxCout; 
 
 class Monitor {
 
 private:
+  // PALÉ DEL SUMINISTRADOR
+  static const int tamPila=3;
+  
+  struct Pila {
+
+    mutex mtxPila;
+  
+    int p [ tamPila ];
+    int ind=0; //apunta siempre a la última posición vacía 
+    const int ERROR=-1;
+  
+
+    void escribir ( int n) {
+      mtxPila.lock();
+      if ( ind < tamPila ) {
+	p [ind++]=n; 
+      }
+      mtxPila.unlock();
+    
+    }
+
+    int leer() {
+
+      mtxPila.lock();
+      int ret=(ind > 0 )?p[ --ind] : ERROR ;
+      mtxPila.unlock();
+      return ret; 
+    }
+  
+  };
+
+  Pila pale;
+
 
   //inicializamos estanqueros y fumadores sin suministros
-  Semaphore sReponer = 1; //al comenzar se repone a un estanquero
-  Semaphore sMostrador = 1; //mostrador vacío
-  Semaphore estanquero[numEstanqueros]={0,0};
-  Semaphore sFumador[numFumadores]=  {0,0,0,0}; 
-
-  int elementosConsumidos = 0; //número de elementos que han consumido 
-  const int numSuministros = 2;    // número de elementos que se recargan tras cada gestion
-  const int finGestor = 3;  //número de elementos gestionados a los que se acaba
-
-  mutex mtxElementosConsumidos;
+  int numLote=0; 
+  Semaphore sReponer = tamPila;  //huecos vacío de la pila de lotes 
+  Semaphore sMostrador = 1; //mostrador vacío indica 1
+  Semaphore estanquero[numEstanqueros]={0};
+  Semaphore sFumador[numFumadores]=  {0,0,0}; 
   
 public:
 
   /**
      Repone a un estanquero aleatorio @param num_suministros un número concreto de elementos
    */
-  void reponer( ) {
+  void suministra( ) {
 
     sem_wait( sReponer );
     
-    int n_estanquero = aleatorio <0,(numEstanqueros-1)>();
-
     //tiempo que tarda de gestion
-     chrono::milliseconds duracion_reponer( aleatorio<20,200>() );
-     mtxCout.lock();
-     cout << "\nGESTOR( " <<  duracion_reponer.count()
-	  << "ms) repone al estanquero " << n_estanquero
-	  << "con " << numSuministros<< "elementos"<<endl << flush;
-     mtxCout.unlock();
-     this_thread::sleep_for( duracion_reponer);
+    chrono::milliseconds duracion_reponer( aleatorio<20,200>() );
+    this_thread::sleep_for( duracion_reponer);
+    mtxCout.lock();
+    cout << "La suministradora añande el lote " <<numLote<< " a la pila " << endl << flush;
+    mtxCout.unlock();
 
-     //damos suministros al estanquero
-     for (int i=0; i< numSuministros; i++)
-       sem_signal(estanquero[n_estanquero] );   
+    pale.escribir( numLote++); 
+     
+     
+
+    //indicamos al estanquero nuevos suministros en el palé
+    sem_signal(estanquero[0] );   
   }
 
   /**
@@ -83,25 +111,25 @@ public:
     sem_wait( estanquero[n_estanquero]); // tiene sumisnistros 
     sem_wait( sMostrador); // comprobamos que esté vacío
 
+    mtxCout.lock();
+    cout << " Estanquero coge el lote  "<< pale.leer()<< endl<<flush;
+    mtxCout.unlock();
+    sem_signal( sReponer);
+    
     int elemento = aleatorio <0,(numFumadores-1)>();
 
     //tiempo que tarda en reponer
-     chrono::milliseconds duracion_poner( aleatorio<20,200>() );
-     this_thread::sleep_for( duracion_poner);
-     mtxCout.lock();
-     cout << " \tMOSTRADOR estanquero "<< n_estanquero<<") " <<  duracion_poner.count()
-	  << "ms coloca elemento " << elemento << " en el mostrador "<< endl<<flush;
-     mtxCout.unlock();
+    chrono::milliseconds duracion_poner( aleatorio<20,200>() );
+    this_thread::sleep_for( duracion_poner);
+    mtxCout.lock();
+    cout << "Estanquero coloca elemento " << elemento << " en el mostrador "<< endl<<flush;
+    mtxCout.unlock();
      
-     //activamos fumador correspondiente
-     sem_signal( sFumador[elemento] );
+    //activamos fumador correspondiente
+    sem_signal( sFumador[elemento] );
     
-     //veamos si se necesita volver a reponer
-     mtxElementosConsumidos.lock(); //zona crítica
-     if ( ++elementosConsumidos % numSuministros == 0 ) {
-       sem_signal( sReponer); 
-     }
-     mtxElementosConsumidos.unlock(); 
+    //veamos si se necesita volver a reponer
+
     
   }
 
@@ -110,7 +138,7 @@ public:
     sem_wait ( sFumador[n_fumador]); // hay en el mostrador algo para él
 
     mtxCout.lock();
-    cout << "fumador " << n_fumador << "coge objeto del mostrador" << endl; 
+    cout << "Fumador " << n_fumador << " coge objeto del mostrador" << endl; 
     sem_signal( sMostrador);
     mtxCout.unlock();
     
@@ -119,13 +147,14 @@ public:
 
    // informa de que comienza a fumar
    mtxCout.lock();
-   cout << "\t\t FUMAR: Fumador " << n_fumador 	<< " empieza a fumar"<< endl;
+   cout << "fumador " << n_fumador << " empieza a fumar"<< endl;
    mtxCout.unlock();
    
    // espera bloqueada un tiempo igual a ''duracion_fumar' milisegundos
    this_thread::sleep_for( duracion_fumar );
-    mtxCout.lock();
-   cout << "\t\tFumador " << n_fumador 	<< " ha terminado fumar ("
+   
+   mtxCout.lock();
+   cout << "Fumador " << n_fumador 	<< " ha terminado fumar ("
 	<<duracion_fumar.count()<< " ms)"<< endl;
    mtxCout.unlock();
    
@@ -137,12 +166,7 @@ public:
    */
 
   bool fin() {
-    //NUNCA ACABA  ret = false
-    /**
-    mtxElementosConsumidos.lock(); 
-    bool ret = !( elementosConsumidos < finGestor );
-    mtxElementosConsumidos.unlock();
-    */
+    //Como el enunciado pide un bucle infinito en esta condición aparecerá devolverá siempre que nunca aca
     bool ret = false; 
     return ret; 
   }
@@ -154,9 +178,11 @@ Monitor gestor;
 //----- funciones  hebra --------
 
 void funcion_hebra_fumador( int id) {
-  while ( !gestor.fin()) {
+  while ( !gestor.fin()) {  // gestor.fin() siempre devuelve false ya que nos están pidiendo un bucle infinito
     gestor.fumador(id); 
   }
+
+  
   mtxCout.lock();
   cout << "\t\t\tFIN fumador " << id << " se cansa de fumar"<< endl; 
   mtxCout.unlock();
@@ -165,13 +191,18 @@ void funcion_hebra_fumador( int id) {
 void funcion_hebra_estanquero( int id ) {
   while (!gestor.fin())
     gestor.poner( id);
+
+  
   mtxCout.lock();
   cout << "\t\t\tFIN Estanquero "<< id << " ya no le quedan recursos"<< endl; mtxCout.unlock();
 }
 
 void funcion_hebra_gestor() {
   while (!gestor.fin() )
-    gestor.reponer();
+    gestor.suministra();
+
+
+  git 
   mtxCout.lock();
   cout << "\t\t\tFIN El gestor ya no tiene más abastos" << endl;
   mtxCout.unlock();
@@ -184,7 +215,7 @@ void funcion_hebra_gestor() {
 int main() {
 
   //creamos hebras 
-  thread hGestor ( funcion_hebra_gestor);
+  thread suministradora ( funcion_hebra_gestor);
 
   thread hEstanquero[ numEstanqueros],hFumadores[numFumadores];
   for ( int i=0; i<numEstanqueros; i++)
@@ -193,13 +224,6 @@ int main() {
    for ( int i=0; i<numFumadores; i++)
     hFumadores[i] = thread( funcion_hebra_fumador, i);
 
-   //esperamos hebras
-   /**
-   for ( int i=0; i<numFumadores; i++)
-     hFumadores[i].join();
-   
-   for ( int i=0; i<numEstanqueros; i++)
-     hEstanquero[i].join(); 
-   */
-  hGestor.join(); 
+
+  suministradora.join(); 
 }
