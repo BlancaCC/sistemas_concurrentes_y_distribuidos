350b2ed3ee78487d1c407b1f26498658c719e595
Blanca <blancacano@correo.ugr.es>
2019-11-13 09:28:47 +0100

    [semantica/variaciones/ dosBarberos y tijeras] - Creo programilla para múltiples barberos, sillas y clientes - Creo programilla para barbero que necesita tijeras de un proveedor para seguir cortando

--- semantica/variaciones/dosBarberos.cpp
+++ semantica/variaciones/dosBarberos.cpp
@@ -1,18 +1,19 @@
 /**
-   Problema del barbero durmiente 
+   Problema del barbero durmiente con varias sillas, barberos y clientes
 
 @author: Blanca Cano Camarero 
-@date: 30 / X / 19 
+@date: 13 / XI / 19 
 
-- Hay un barbero que pela
-- Varios clientes
+- Hay Varios Barberos
+- Hay varios clientes
+- Hay varias sillas
 
 Condiciones: 
-- Un cliente entra a la barbería, si el sillón de pelar está desierto te sienta, si el babero está durmiendo lo despierta
+- Un cliente entra a la barbería, si el sillón de pelar está desierto te sienta, si el babero está durmiendo lo despierta para que lo atienda, y si todos los barberos están trabajando espera a que le atiendan
 
 - Un barbero pela si hay alguien en la silla de pelar, si no lo llama de la sala de espera, si tampoco lo hay ahí se echa dormir
 
-- Tras pelar el barberlo busca a otro cliente 
+- Tras pelar el barbero busca a otro cliente 
 - Tras ser pelado el cliente espera fuera de la barbería. 
 
 
@@ -42,53 +43,64 @@ private:
   CondVar barbero = newCondVar(); //ell barbero espera cuando duerme, es decir no hay clientes que pelar
   CondVar sillonPelar = newCondVar(); //mientras el cliente está siendo pelado
 
+  int numBarberos, numSillas;
+  int barberosTrabajando = 0;
+
 
 public:
 
   //tiempo en que se está cortando el pelo el cliente
+  Barberia(int nb,int ns):numBarberos(nb),numSillas(ns){};
   void cortarPelo(int id);
-  void siguienteCliente(); // el barbero se pone a pelar a otro cliente
-  void finCliente(); // fin del cliente que está pelado 
+  void siguienteCliente(int id); // el barbero se pone a pelar a otro cliente
+  void finCliente(int id); // fin del cliente que está pelado 
   
 };
 
 
-void Barberia::siguienteCliente() {
+void Barberia::siguienteCliente(int id) {
 
   // si no hay cliente esperando a ser pelado en el sillón 
-  if( sillonPelar.empty() ) {
+  if( sillonPelar.get_nwt() == barberosTrabajando) { //el número de barberos atendiendo (que no sea yo) es menor que la sillas a espensas des ser atendidas
     // si no hay cliente en la sala de espera esperando a ser pelado
     if(salaEspera.empty()) {
       //bloqueamos al barbero
-      cout << "No hay clientes el barbero se duerme "<< endl;
+      cout << "No hay clientes el barbero " << id <<" se duerme "<< endl;
       barbero.wait();
     }
     else {
       // sí hay un cliente esperando en al sala de espera
-      cout << "Barbero llama a un cliente de la sala de espera" << endl; 
+      cout << "Barbero " << id <<" llama a un cliente de la sala de espera" << endl; 
       salaEspera.signal(); //lo saca para el sillón de pelar 
     } 
   }
+barberosTrabajando++;
 }
 
 void Barberia::cortarPelo(int id) {
+
+  // DESPERTAR BARBERO
+  // hay sillas libres, todos los barberos activos están atendiendo y quedan barberos descansando
+  int sillas = sillonPelar.get_nwt();
   
-  if(!barbero.empty()) { // si el barbero está dormido lo despertamos
-    cout << "El cliente " << id << " despierta al barbero y pasa directamente a pelarse " << endl; 
+  if( sillas< numSillas && sillas == barberosTrabajando && !barbero.empty()) {
+    //if(barbero.get_nwt() == numBarberos){ // si ambos barberos están dormidos 
+    cout << "El cliente " << id << " despierta a uno de los  barberos y pasa directamente a pelarse " << endl; 
     barbero.signal();
   }
   
-  //mientras halla alguien pelándose
-  else if(!sillonPelar.empty()) {
+  //mira si quedan sillas libres donde sentarse
+  else if (sillas == numSillas) {
     cout << "Clinte " << id << " en sala de espera " << endl;
     salaEspera.wait(); // se espera en la sala de espera
   }
 
-  cout << "Cliente " << id << " ocupa sillón de pelar" << endl;
+  cout << "Cliente " << id << " ocupa sillón de pelar" << endl
+       << "barberos durmiendo " << barbero.get_nwt() << endl;
 
   sillonPelar.wait(); //ocupa el sillón de pelar
 
-  cout << "\t\t\t\tlienten " << id << " marcha pelado " << endl<< endl; 
+  cout << "\t\t\tCliente " << id << " marcha pelado " << endl; 
 }
 /**
    Tiempo en que el cliente @param id está detenido 
@@ -114,13 +126,15 @@ void CortarPeloCliente() {
 
   this_thread::sleep_for(duracion);
 
-  //cout << "El barbero ha tardado " << duracion.count() << " en despachar al cliente  " << endl; 
+  //cout << "El barbero ha tardado " << duracion.count()  << " en despachar al cliente  " << endl; 
 }
 
 
-void Barberia::finCliente() {
+void Barberia::finCliente(int id) {
   sillonPelar.signal();
-  cout << "\t\t\t\tEl barbero ha despachado al cliente " << endl; 
+  cout << "\t\t\t\t El barbero " << id <<" ha despachado a su cliente " << endl
+       << "\t\t\t\t \tsillas ocupadas :" << sillonPelar.get_nwt() << endl;
+  barberosTrabajando--;
 }
 
 /**
@@ -135,29 +149,42 @@ void Cliente(int id, MRef<Barberia>Mbarberia){
 }
 
 
-void Barbero(MRef<Barberia>Mbarberia){
+void Barbero(MRef<Barberia>Mbarberia, int id){
   while(true){
-    Mbarberia->siguienteCliente();
+    Mbarberia->siguienteCliente(id);
     CortarPeloCliente();
-    Mbarberia->finCliente();
+    Mbarberia->finCliente(id);
   }
 }
 
 
 int main(){
 
-  const int N_CLIENTES=10; 
+  const int N_CLIENTES=4;
+  const int N_BARBEROS=2;
+  const int N_SILLAS = 3;
   cout << " --------- PROBLEMA DEL BARBERO DURMIENTE ---------- " << endl
        << "Nº clientes: " << N_CLIENTES << endl
+       << "Número de barberos " << N_BARBEROS << endl
+       << "Nº de sillas " << N_SILLAS << endl
        << " ----------------------------------------------------" << endl;
 
-  MRef<Barberia> Mbarberia = Create<Barberia>();
-  thread barbero(Barbero, Mbarberia);
+  MRef<Barberia> Mbarberia = Create<Barberia>(N_BARBEROS,N_SILLAS);
+  //thread barbero(Barbero, Mbarberia);
+  
   thread clientes[N_CLIENTES];
+  thread barberos[N_BARBEROS];
+
   
   for(int i=0; i<N_CLIENTES; i++)
     clientes[i]=thread(Cliente, i, Mbarberia);
 
-  barbero.join();
+  for(int i=0; i<N_BARBEROS; i++)
+    barberos[i]=thread(Barbero, Mbarberia, i);
+
+  barberos[0].join(); //no queremos que el programa no acabe nunca 
+
+
+  
   return 0; 
 }
--- semantica/variaciones/tijeras.cpp
+++ semantica/variaciones/tijeras.cpp
@@ -1,10 +1,10 @@
 /**
-   Problema del barbero durmiente 
+   Problema del barbero durmiente con tijeras
 
 @author: Blanca Cano Camarero 
-@date: 30 / X / 19 
+@date: 13 / XI / 19 
 
-- Hay un barbero que pela
+- Hay un barbero que pela con una tijeras
 - Varios clientes
 
 Condiciones: 
@@ -35,23 +35,91 @@ template< int min, int max > int aleatorio()
 }
 
 
+class SuministroTijeras : public HoareMonitor {
+private:
+  CondVar barberoEsperaTijeras = newCondVar();
+  CondVar bufferLLeno = newCondVar(); //no queda hueco para traer más tijeras
+
+  int tamPaquete = 0; 
+
+public:
+
+  //barbero coge tijeras
+  int cogerTijeras();
+  void ponerTijeras(int i);
+  
+};
+
+
+void SuministroTijeras::ponerTijeras(int n) {
+  //comprueba si es necesario recargar
+  if(tamPaquete != 0) {
+    cout << " Sumintrador de tijeras no puede poner más porque el buffer está lleno" << endl; 
+    bufferLLeno.wait();
+  }
+  tamPaquete = n;
+  cout << "Suministrado de tijeras coloca " << n << " nuevas " << endl;
+  if( !barberoEsperaTijeras.empty())
+    barberoEsperaTijeras.signal();
+}
+int SuministroTijeras::cogerTijeras() {
+  int ret;
+  if(tamPaquete == 0) {
+    cout << "No hay tijeras en el almacén el barbero espera" << endl; 
+    barberoEsperaTijeras.wait(); //espera a que venga el paquete
+  }
+  ret = tamPaquete;
+  tamPaquete = 0;
+  
+  if(!bufferLLeno.empty()) {
+    cout << "Barbero avisa al repartidos de que ya puede volver a reponer" << endl; 
+    bufferLLeno.signal();
+  }
+  cout << "Barbera marcha a la peluquería a seguir con sus quehaceres peluqueros " << endl; 
+  return ret;
+}
+
+
+//clase barbería control; clientes y acción de pelar 
 class Barberia : public HoareMonitor {
 
 private:
   CondVar salaEspera = newCondVar();
-  CondVar barbero = newCondVar(); //ell barbero espera cuando duerme, es decir no hay clientes que pelar
+  CondVar barbero = newCondVar(); //el barbero espera cuando duerme, es decir no hay clientes que pelar
   CondVar sillonPelar = newCondVar(); //mientras el cliente está siendo pelado
 
+  int numTijeras = 0; //número de tijeras que tiene almacenadas 
+  int numUsos = 0;    //número de usos que lleva con la tijera actual
+  const int maxUsosTijeras = 2;
+  
+  void usarTijeras(); //función que gestiona el uso de las tijeras
 
 public:
 
   //tiempo en que se está cortando el pelo el cliente
   void cortarPelo(int id);
   void siguienteCliente(); // el barbero se pone a pelar a otro cliente
-  void finCliente(); // fin del cliente que está pelado 
+  void finCliente(); //fin del cliente que está pelado
+  bool barberoTieneTijeras(); //true si el barbero tiene suficientes tijeras pasa seguir
+  void recargarTijeras(int n);
   
 };
 
+void Barberia::recargarTijeras(int n) {
+  cout << "Barbero recarga " << n << "tijeras nuevas " << endl; 
+  numTijeras = n;
+}
+void Barberia::usarTijeras() {
+  numUsos++;
+  if(numUsos == maxUsosTijeras) {
+    numTijeras--;
+    numUsos = 0;
+  }
+}
+
+bool Barberia::barberoTieneTijeras() {
+  return !(numTijeras == 0);
+}
 
 void Barberia::siguienteCliente() {
 
@@ -88,7 +156,7 @@ void Barberia::cortarPelo(int id) {
 
   sillonPelar.wait(); //ocupa el sillón de pelar
 
-  cout << "\t\t\t\tlienten " << id << " marcha pelado " << endl<< endl; 
+  cout << "\t\tClienten " << id << " marcha pelado " << endl<< endl; 
 }
 /**
    Tiempo en que el cliente @param id está detenido 
@@ -99,11 +167,18 @@ void EsperarFueraBarberia(int id) {
 
   this_thread::sleep_for(duracion);
 
-  cout << "Tras " << duracion.count() << " el ciente " << id
+  cout << "Tras " << duracion.count() << " el cliente " << id
        << " se dirige a la barbería " << endl; 
 }
 
 
+void irAPorMasSuministros() {
+    chrono::milliseconds duracion(aleatorio<30,1000>());
+
+    this_thread::sleep_for(duracion);
+    
+     cout << "El proveedor ha tardado  " << duracion.count() << " en ir a por tijeras " << endl; 
+}
 /**
    Cortar pelo tiempo en que el barbero está cotando el pelo 
  */
@@ -120,7 +195,8 @@ void CortarPeloCliente() {
 
 void Barberia::finCliente() {
   sillonPelar.signal();
-  cout << "\t\t\t\tEl barbero ha despachado al cliente " << endl; 
+  usarTijeras();
+  cout << " El barbero ha despachado al cliente " << endl; 
 }
 
 /**
@@ -135,29 +211,44 @@ void Cliente(int id, MRef<Barberia>Mbarberia){
 }
 
 
-void Barbero(MRef<Barberia>Mbarberia){
+void Barbero(MRef<Barberia>Mbarberia, MRef<SuministroTijeras>Mtijeras){
   while(true){
+    //comprobamos que tiene material suficiente para trabajar
+    if(! Mbarberia->barberoTieneTijeras()){
+      Mbarberia->recargarTijeras( Mtijeras->cogerTijeras());
+    }
     Mbarberia->siguienteCliente();
     CortarPeloCliente();
     Mbarberia->finCliente();
   }
 }
 
+void Proveedor(MRef<SuministroTijeras>Mtijeras){
 
+  while(true) {
+    
+  Mtijeras->ponerTijeras(1);
+  irAPorMasSuministros();
+  }
+  
+}
 int main(){
 
   const int N_CLIENTES=10; 
-  cout << " --------- PROBLEMA DEL BARBERO DURMIENTE ---------- " << endl
+  cout << " --------- PROBLEMA DEL BARBERO DURMIENTE CON TIJERAS ---------- " << endl
        << "Nº clientes: " << N_CLIENTES << endl
        << " ----------------------------------------------------" << endl;
 
   MRef<Barberia> Mbarberia = Create<Barberia>();
-  thread barbero(Barbero, Mbarberia);
+  MRef<SuministroTijeras>Mtijeras = Create<SuministroTijeras>();
+  
+  thread barbero(Barbero, Mbarberia,Mtijeras);
   thread clientes[N_CLIENTES];
   
   for(int i=0; i<N_CLIENTES; i++)
     clientes[i]=thread(Cliente, i, Mbarberia);
 
+  thread proveedor(Proveedor, Mtijeras);
   barbero.join();
   return 0; 
 }
